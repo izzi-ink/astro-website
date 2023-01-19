7b486305998df5a5db86babc19fa3e39895e1153
Izzi <izzikoning@me.com>
2023-01-19 19:58:45 +0200

    Example of React Component for the Showcase

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import react from "@astrojs/react";
+
+// https://astro.build/config
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react()]
+});
